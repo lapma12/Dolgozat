627236db5e4c4313330ff9271b5d2fb77adf1442
lapma12 <lapostyanm@kkszki.hu>
2025-11-24 08:03:24 +0100

    Proterties are created.

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -3,5 +3,14 @@ namespace Dolgozat.Models
 {
     public class Computer
     {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        
+        public string Type { get; set; }
+
+        public double Display { get; set; }
+        public int Memory { get; set; }
+
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
     }
 }
